80762f8debf15b320e1bbba4e68a7e60ec361a5c
verma04 <codepankaj04@gmail.com>
2023-10-12 16:26:20 +0530

    added

--- alumniThrive/screen/dashboard/Home/Dashboard.tsx
+++ alumniThrive/screen/dashboard/Home/Dashboard.tsx
@@ -1,7 +1,10 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {Pressable, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {Header} from '@rneui/themed';
-const Dashboard = () => {
+import BellSvg from '../../../svg/BellSvg';
+import HamburgerSvg from '../../../svg/HamburgerSvg';
+import {Image} from 'react-native-elements';
+const Dashboard = ({navigation}) => {
   return (
     <ScrollView style={{flex: 1}}>
       <View style={{flex: 1, display: 'flex', justifyContent: 'center'}}>
@@ -9,13 +12,32 @@ const Dashboard = () => {
           backgroundImageStyle={{}}
           barStyle="default"
           centerContainerStyle={{}}
-          containerStyle={{width: '100%'}}
-          leftComponent={<Text>sddsd</Text>}
+          containerStyle={{width: '100%', backgroundColor: 'white'}}
+          leftComponent={
+            <Text>
+              <Image
+                source={require('../../../assets/hamBurgerLogo.png')}
+                style={{width: 30, height: 30}}
+              />
+            </Text>
+          }
           leftContainerStyle={{}}
           linearGradientProps={{}}
           placement="center"
-          rightComponent={{icon: 'home', color: '#fff'}}
-          rightContainerStyle={{}}
+          rightComponent={
+            <View
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                gap: 10,
+                flexDirection: 'row',
+              }}>
+              <BellSvg />
+              <Pressable onPress={() => navigation.openDrawer()}>
+                <HamburgerSvg />
+              </Pressable>
+            </View>
+          }
           statusBarProps={{}}
         />
       </View>
