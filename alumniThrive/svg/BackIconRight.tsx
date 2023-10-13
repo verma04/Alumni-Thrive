050774e024b041cc12288fc169fd5673981844a1
verma04 <codepankaj04@gmail.com>
2023-10-13 12:37:14 +0530

    added

---
+++ alumniThrive/svg/BackIconRight.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Path, Svg} from 'react-native-svg';
+
+const BackIconRight = () => {
+  return (
+    <Svg width="10" height="17" viewBox="0 0 10 17" fill="none">
+      <Path
+        d="M1.00016 1.28255L8.0835 8.36589L1.00016 15.4492"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </Svg>
+  );
+};
+
+export default BackIconRight;
